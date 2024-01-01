198b6509974994cbdba1b7c2b9e0c50d1d54b037
vitor-pelicer <vpm.franca@unesp.br>
2024-01-01 20:07:32 -0300

    feat: update request api

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,7 @@
 const { ObjectId } = require("mongodb");
 
 const endpoint = '/notes'
-const collection = 'notes'
+const collectionName = 'notes'
 
 module.exports = function(app, db){
   app.get(endpoint, async (req, res) => {
@@ -13,7 +13,7 @@ module.exports = function(app, db){
     if(title){
       query.title = title;
     }
-    const cursor = db.collection(collection)
+    const cursor = db.collection(collectionName)
       .find(query);
     const notes = [];
     for await (const doc of cursor) {
@@ -24,7 +24,7 @@ module.exports = function(app, db){
 
   app.post(endpoint, async (req, res) => {
     const note = { text: req.body.body, title: req.body.title }
-    db.collection(collection).insertOne(note)
+    db.collection(collectionName).insertOne(note)
       .then((result) => {
         console.log(result);
         res.status(200).send("ok");
@@ -44,7 +44,7 @@ module.exports = function(app, db){
     if(title){
       query.title = title;
     }
-    db.collection(collection)
+    db.collection(collectionName)
       .deleteOne(query)
         .then((result) => {
           console.log(result);
@@ -54,5 +54,41 @@ module.exports = function(app, db){
           console.log(err);
           res.status(400).send("erro");
         });
+  })
+
+  app.patch(endpoint, async (req, res) => {
+    if(!req.body.body && !req.body.title){
+      res.status(400).send("Body and title is missing");
+    }
+    const note = { text: req.body.body, title: req.body.title }
+    const {id, title} = req.query;
+    var query = {}
+    if(id){
+        query._id = new ObjectId(id);
+    }
+    if(title){
+      query.title = title;
+    }
+    const collection = db.collection(collectionName);
+    var cursor = collection.find(query);
+    const notes = [];
+    for await (const doc of cursor) {
+      notes.push(doc);
+    }
+    if(notes[0]){
+      const document = notes[0]
+      if (note.text) document.text = note.text;
+      if (note.title) document.title = note.title
+      console.log(notes[0]);
+      collection.replaceOne(query, document)
+        .then((result) => {
+          console.log(result);
+          res.status(200).send("ok");
+        })
+        .catch((err)=>{
+          console.log(err);
+          res.status(400).send("erro");
+        });
+    } else res.status(400).send("Não encontrado");
   })
 }
